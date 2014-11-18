1d4a1efabc91425240addae76d463c396bd95662
Cuong Hoang <cuong.hoang@jobseeker.com.au>
2014-11-18 12:10:53 +1100

    Initialise Phrase after metal initializer to fix conflicts Teaspoon

--- lib/phrase/engine.rb
+++ lib/phrase/engine.rb
@@ -5,7 +5,7 @@ require 'i18n'
 
 if defined? Rails
   class Phrase::Engine < Rails::Engine
-    initializer 'phrase', :after => :disable_dependency_loading do |app|
+    initializer 'phrase', :after => :initialize_metal do |app|
       require 'phrase/adapters/i18n'
       require 'phrase/adapters/fast_gettext'
       ActionView::Base.send :include, Phrase::ViewHelpers
